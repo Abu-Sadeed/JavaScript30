5765ccb0105cf2baf2cc1ee81b8093cc87b2ee15
KillerQueen-BitesZaDusto <abusadeed@iut-dhaka.edu>
2020-07-01 14:22:49 +0600

    Created image slide in with scroll

---
+++ 13 - Slide in on Scroll/script.js
@@ -0,0 +1,33 @@
+function debounce(func, wait = 20, immediate = true){
+    var timeout;
+    return function(){
+        var context = this, args = arguments;
+        var later = function(){
+            timeout = null;
+            if (!immediate) func.apply(context, args);
+        };
+        var callNow = immediate && !timeout;
+        clearTimeout(timeout);
+        timeout = setTimeout(later, wait);
+        if (callNow) func.apply(context, args);
+    };
+}
+
+const sliderImages = document.querySelectorAll(".slide-in");
+
+function checkSlide(e){
+    sliderImages.forEach(sliderImage =>{
+        const slideIn = (window.scrollY + window.innerHeight) - sliderImage.height / 2;
+        const imageBottom = sliderImage.offsetTop + sliderImage.height;
+        const isHalfShown = slideIn > sliderImage.offsetTop;
+        const isNotScrolled = window.scrollY < imageBottom;
+
+        if(isHalfShown && isNotScrolled){
+            sliderImage.classList.add("active");
+        }else{
+            sliderImage.classList.remove("active");
+        }
+    })
+}
+
+window.addEventListener("scroll", debounce(checkSlide));
